e1803e4c65b0c3b46d25239dac65b032fd29c72c
infi-bins-loop <mbsamee0108@gmail.com>
2023-10-24 20:20:51 +0530

    initial push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Result from './components/Result';
+import KeyPad from './components/KeyPad';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    result: ""
+  }
+
+  onClick = button => {
+    if(button === "=") {
+      this.calculate();
+    }
+
+    else if(button === "C") {
+      this.reset();
+    }
+
+    else if(button === "CE") {
+      this.backspace();
+    }
+
+    else {
+      this.setState({
+        result: this.state.result + button
+      })
+    }
+  };
+
+  calculate = () => {
+    var checkResult = ''
+    if(this.state.result.includes('--')) {
+      checkResult = this.state.result.replace('--', '+')
+    } else {
+      checkResult = this.state.result;
+    }
+
+    try {
+      this.setState({
+        result: (eval(checkResult) || "") + ""
+      })
+    } catch(e) {
+      this.setState({
+        result: "error"
+      })
+    }
+  };
+
+  reset = () => {
+    this.setState({
+      result: ""
+    })
+  };
+
+  backspace = () => {
+    this.setState({
+        result: this.state.result.slice(0, -1)
+    })
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <div className="calculator-body">
+          <Result result={this.state.result} />
+          <KeyPad onClick={this.onClick} />
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
